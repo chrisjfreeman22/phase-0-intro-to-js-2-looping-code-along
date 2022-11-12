2674766182390a094c3aea7840f6d385e35eb6f9
Chris Freeman <chrisjfreeman22@gmail.com>
2022-11-11 18:33:56 -0600

    commit tree d76f696ed872562cd2a9d443bbd03f0fd4a70f7f

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(nameArray, eventName){
+    let newArrayOfMessages = []
+    for (let i=0;i<nameArray.length;i++){
+        console.log(i)
+        newArrayOfMessages.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`)
+        console.log(newArrayOfMessages)
+    }
+  return newArrayOfMessages
+}
+function countDown(CountDownStartNumber){
+    let x = CountDownStartNumber
+    while (x != -1){
+        console.log(x)
+        x--
+    }
+}
+writeCards(["tony","zaret"],"monday girlfriend")
+countDown(20)
